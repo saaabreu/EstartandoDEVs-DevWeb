d7576d5086cea229833717a15b1795bd2c8fcadb
Sarah <sarah.abreu2012@gmail.com>
2020-10-08 13:41:49 -0300

    Exercícios JS

---
+++ Exercícios-JS/ex01.js
@@ -0,0 +1,7 @@
+/* Para passar no Colegio "Crias do Devs" os alunos precisam de media 8. Crie um código que tenha uma variavel com o valor da media do aluno no semestre retorne true ou false de acordo com o resultado do aluno no colegio. */
+
+var nota1 = 8;
+var nota2 = 8;
+var media = (nota1 + nota2) / 2
+var aprovacao = media >= 8
+console.log('Média:',media, 'Status da aprovação:', aprovacao)
---
+++ Exercícios-JS/ex02.js
@@ -0,0 +1,20 @@
+/* Verifique se as afirmativas estão certas, caso contrario justifique com uma prova real. Dica: Use o typeof para as provas reais. */
+
+// A) O tipo da constante `typeNumber` é number. False
+const typeNumber = "10";
+console.log(typeof typeNumber)
+
+
+// B) O valor da constante `y` é 149. False
+const y = 147 + "2";
+console.log(y)
+
+
+// C) O tipo da constante `x` é number. False
+const x = 13 > 20;
+console.log(typeof x)
+
+
+// D) O valor da constante `s` é "Estartando Devs". True
+const s = "Estartando " + "Devs";
+console.log(s)
